80ea6e92e9e498b96bd58b5644d07b27a7a78f7d
Mohit Deshpande <mohitdeshpande@icloud.com>
2014-11-28 15:34:11 +0530

    Set up initial scene
    Set up the background. the main screen, it’s size and behaviour

--- Tetris/GameScene.swift
+++ Tetris/GameScene.swift
@@ -11,6 +11,24 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    required init(coder aDecoder: NSCoder!) {
+        fatalError("NSCoder not supported")
+    }
+    
+    override init(size: CGSize) {
+        super.init(size: size)
+        
+        // Since tetris will be drawn from the top down so we anchor
+        // out game in the top-left corner of the sceen ie. 0, 1.0
+        anchorPoint = CGPoint(x: 0, y: 1.0)
+        
+        let background = SKSpriteNode(imageNamed: "background")
+        background.position = CGPoint(x: 0, y: 0)
+        background.anchorPoint = CGPoint(x: 0, y: 1.0)
+        addChild(background)
+
+    }
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
